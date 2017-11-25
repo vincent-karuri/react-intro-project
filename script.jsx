f78f7a76abf3ea1fef00ac921fcb8fa32a815e44
vkaruri <vkaruri@princeton.edu>
2017-11-25 07:01:40 -0500

    pass parent component function to child component

--- script.jsx
+++ script.jsx
@@ -7,11 +7,16 @@ var Product = React.createClass({
     this.setState({qty: this.state.qty + 1});
   },
   
+  show: function() {
+    this.props.handleShow(this.props.name);
+  },
+  
   render: function() {
     return (
       <div>
         <p>{this.props.name} - ${this.props.price}</p>
         <button onClick={this.buy}>Buy</button>
+        <button onClick={this.show}>Show</button>
         <h3>Qty: {this.state.qty} item(s)</h3>
         <hr/>
       </div>
@@ -30,12 +35,19 @@ var Total = React.createClass({
 });
 
 var ProductList = React.createClass({
+  showProduct : function(name) {
+    alert("You selected " + name);
+  },
+  
   render: function() {
     return (
       <div>
-        <Product name="Android" price="123"/>
-        <Product name="Apple" price="321"/>
-        <Product name="Nokia" price="432"/>
+        <Product name="Android" price="123" 
+            handleShow={this.showProduct}/>
+        <Product name="Apple" price="321" 
+            handleShow={this.showProduct}/>
+        <Product name="Nokia" price="432" 
+            handleShow={this.showProduct}/>
         <Total/>
       </div>  
     );
