157fe24d57dd91f346a1e6c0c05b83937ccacffd
vkaruri <vkaruri@princeton.edu>
2017-11-25 06:29:44 -0500

    Initial commit. Shows reuse of Product component and setting of props inside the parent ProductList class

---
+++ script.jsx
@@ -0,0 +1,45 @@
+var Product = React.createClass({
+  getInitialState: function() {
+    return {qty: 0};  
+  },
+  
+  buy: function() {
+    this.setState({qty: this.state.qty + 1});
+  },
+  
+  render: function() {
+    return (
+      <div>
+        <p>{this.props.name} - ${this.props.price}</p>
+        <button onClick={this.buy}>Buy</button>
+        <h3>Qty: {this.state.qty} item(s)</h3>
+        <hr/>
+      </div>
+    );
+  }
+});
+
+var Total = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3>Total Cash: </h3>
+      </div>  
+    );
+  }
+});
+
+var ProductList = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Product name="Android" price="123"/>
+        <Product name="Apple" price="321"/>
+        <Product name="Nokia" price="432"/>
+        <Total/>
+      </div>  
+    );
+  }
+});
+
+React.render(<ProductList/>, document.getElementById("root"));
